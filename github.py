a947704c513a6408c0cc456b534c81e7b601e17a
eaguilarf21 <eaguilarf@certus.edu.pe>
2022-05-11 09:48:09 -0500

    mi primera vez con github

---
+++ github.py
@@ -0,0 +1,15 @@
+#
+print("ingrese el numero entre 1 y 7")
+num = int(input())
+
+#
+if num ==1:
+    print("lunes")
+elif num==2:
+    print("martes")
+elif num==3:
+    print("miercoles")    
+else:
+    print("")
+
+#salida
